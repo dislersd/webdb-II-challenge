ab81cb44c2edc8075382ed083ae842fabdeeed5d
dislersd <dislersd@gmail.com>
2019-03-26 14:59:36 -0700

    STRETCH: created bear table and routing for bears - started building end points

---
+++ bear-router.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const knex = require("knex");
+
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  }
+};
+
+const db = knex(knexConfig);
+
+router.get("/", async (req, res) => {
+  try {
+    const bears = await db("bears");
+    res.status(200).json(bears);
+  } catch (error) {
+    res.status(500).json({ message: "error retrieving bears" });
+  }
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("bears")
+    .where({ id })
+    .then(bear => {
+      res.status(200).json(bear);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error getting bear" });
+    });
+});
+
+router.post("/", (req, res) => {
+  db("bears")
+    .insert(req.body)
+    .then(bear => {
+      res.status(201).json(bear);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error posting" });
+    });
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const count = await db("bears").update(req.body).where({id});
+    if (count > 0) {
+      res.status(200).json({message: "success"})
+    } else {
+      res.status(400).json({ message: 'error cannot find' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'error updating' })
+  }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
 const zooRouter = require("./zoo-router.js");
+const bearRouter = require('./bear-router.js')
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use("/zoos", zooRouter)
+server.use("/bears", bearRouter)
 
 server.get("/", (req, res) => {
   res.send("Hello");
